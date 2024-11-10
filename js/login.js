0cfeb7aa171254fdb18b660e573b3888f4a8d674
Akihiko DEAI@JFR <akihiko.deai@jfrgroup.co.jp>
2024-11-10 14:52:00 +0900

    ver3.0

--- js/login.js
+++ js/login.js
@@ -1,28 +1,32 @@
 function checkPassword() {
-  const salt = "68186968";  // セミコロンを追加
-  const pw = "380fac74c0ef488137008f4a0d3558f8132392744a428a7d46d3c02096c0e5cc";
-  const url = "44147512667512335023125979";
+  const salt = ":pw:";  // セミコロンを追加
   const user_idInput = document.getElementById("userid").value;
-  const passwordInput = user_idInput + decodeStr(salt) + document.getElementById("password").value;
+  const passwordInput = document.getElementById("password").value;
+  const xhr = new XMLHttpRequest();
 
-  generateHash(passwordInput).then(encyp => {
-    if (encyp === pw) {
-      document.getElementById("error").style.display = "none";
-      window.location.href = decodeStr(url);
-    } else {
-      document.getElementById("error").style.display = "block";
-    }
+  const orgText = user_idInput + salt + passwordInput;
+  generateHash(orgText).then(encyp => {
+    window.alert(encyp);
+    const url = encyp.substr(0, 8) + encyp.substr(-8, 8);
+    window.alert(url);
+
+    xhr.open('GET', url);
+    xhr.send();
+    xhr.onload = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        window.location = url;
+      } else {
+        const error = document.getElementById('error');
+        const input = document.getElementById('password');
+        error.innerHTML = 'Wrong password! Try again.';
+        input.value = '';
+      }
+    };
+    xhr.onerror = function() {
+      const error = document.getElementById('error');
+      error.innerHTML = 'Sorry, ERROR !';
+    };
   });
-}
-
-function decodeStr(chgStr) {
-  const encodeTbl = "AM{812I=7@]Rtio`bOpH}xVhNKzQ_W09usTU</S3vyZ^c6f>La.rCq|GBk[mYE.g?Pe5:wXDJFjn;4dl";
-  var str = "";
-  for (var i = 0; i < chgStr.length; i += 2) {
-    var idx = Number(chgStr.substring(i, i + 2));
-    str += encodeTbl.substring(idx, idx + 1);
-  }
-  return str;
 }
 
 async function generateHash(password) {
